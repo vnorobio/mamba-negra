4f4325a301baddf0bf78db8e0fb4a4516e9ddd38
vnorobio <vnorobio@gmail.com>
2024-06-09 21:48:48 -0500

    Replace custom password encoder with BCryptPasswordEncoder
    In this change, we have removed the custom password encoder and replaced it with the standard BCryptPasswordEncoder. Moreover, we adjusted the SecurityConfig to create a BCryptPasswordEncoder bean and updated hashing accordingly in data.sql. This change enhances security by using a stronger and more widely-accepted cryptographic algorithm for password hashing.

--- db/sql/data.sql
+++ db/sql/data.sql
@@ -1,4 +1,4 @@
 insert into users (email, password, rol) VALUES
                                                     ('admin_user@mambanegra.com', 'to_be_encoded', 'admin'),
-                                                    ('basic_user@mambanegra', 'to_be_encoded', 'user');
+                                                    ('basic_user@mambanegra', '$2a$10$MOPassbvDHEw.OqNvaN27OpA4aqHOoY5C9opN6r1g3k0kU79Au6oy', 'user');
 
